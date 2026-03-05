110e52cbd3e4bdc65b2a3d32b1362a7f83ed4c73
MikeHedde <mickael.hedde@inrae.fr>
2026-03-05 16:29:43 +0100

    init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' TRES-PRATIC: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Mike Hedde \email{mickael.hedde@inrae.fr}
+#' 
+#' @date 2026/03/05
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
