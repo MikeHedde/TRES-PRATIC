ce12f64ea3a8190eab86abef6ac63a43f7a72175
RomaneBoileau1 <romane.boileau@gmail.com>
2026-03-27 11:17:24 +0100

    essai metadigitise

---
+++ analyses/06_metadigitise.R
@@ -0,0 +1,8 @@
+install.packages("metaDigitise")
+install.packages("devtools")
+devtools::install_github("daniel1noble/metaDigitise")
+
+library(metaDigitise)
+
+
+data <- metaDigitise(dir = "data/articles_corpus/Cardinael_2019")
